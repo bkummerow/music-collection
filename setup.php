1aca2f08028d82dc6f84107f7d7075314620f0c3
Bill Kummerow <bkummerow@emfluence.com>
2025-09-15 07:57:05 -0500

    Made critical css sass and updated package.json; tweaked setup to be more stylistically consistent with collection page

--- setup.php
+++ setup.php
@@ -127,13 +127,11 @@ $displayMode = $settings['display_mode']['theme'];
         <div class="setup-container">
             <header class="setup-header">
                 <div class="setup-header-content">
-                    <h1>Setup & Configuration</h1>
-                    <p>Configure your Discogs API key, authentication, and theme settings</p>
+                    <div class="header-text">
+                        <h1>Setup & Configuration</h1>
+                    </div>
                     <div class="setup-nav">
                         <a href="index.php" class="btn-back">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-                            </svg>
                             Back to Collection
                         </a>
                     </div>
